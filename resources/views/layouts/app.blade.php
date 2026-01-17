dd1f5dcdbb23d57e3f09fffa8c81e5a60e30dbaa
Rexal Toledo <rextoldz@gmail.com>
2026-01-17 18:28:07 +0800

    added font awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             }
         });
     </script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome-local.css' ) }}">
     <style>
         /* The rest style here */
     </style>
--- resources/views/layouts/counter.blade.php
+++ resources/views/layouts/counter.blade.php
@@ -21,6 +21,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="{{ asset('assets/css/all.min.css' ) }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome-local.css' ) }}">
     <style>
         /* Counter-specific styles - no sidebar needed */
         .counter-layout {
