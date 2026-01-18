80bce13ec544f708c38b39bb58327e4324be1dfe
Rexal Toledo <rextoldz@gmail.com>
2026-01-19 01:25:15 +0800

    Revert "Apply tailwind to counter"
    This reverts commit 07ae85e8f7ea4b349e3df7e2c31573d65a64c5fb.

--- resources/views/kiosk/index.blade.php
+++ resources/views/kiosk/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $organization->organization_name ?? 'Queue Kiosk' }} - Queue Kiosk</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
